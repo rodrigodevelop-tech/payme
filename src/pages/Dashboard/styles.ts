24c554efeccff12d0a3bfe48f7c0c1cac75c37b7
rodrigodevelop-tech <digoalves12@hotmail.com>
2022-08-14 23:35:18 -0300

    First commit new branch

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -15,21 +15,28 @@ export const DashboardCards = styled.div`
   margin-top: 3.5rem;
 `;
 
+export const DashboardLineDivider = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-block: 2.125rem;
+  background: ${(props) => props.theme.colors["gray-300"]};
+`;
+
 export const DashboardContent = styled.div`
   width: 100%;
-  margin-top: 1.8125rem;
 `;
 
 export const DashboardContentHeader = styled.header`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 2rem;
 `;
 
 export const DashboardCardsGroupContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  gap: 5rem;
   flex-wrap: wrap;
-  margin-block: 2.125rem;
 `;
